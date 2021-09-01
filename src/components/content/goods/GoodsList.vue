bdcca7f906c881f975858aa04b9d729236087124
wsh <649318629@qq.com>
2021-09-01 12:14:59 +0800

    9.1 中午

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="goods-list">
+    <goods-list-item
+      v-for="(item, index) in goods"
+      :key="index"
+      :goods-item="item"
+    ></goods-list-item>
+    <ul>
+      <li>商品1</li>
+      <li>商品2</li>
+      <li>商品3</li>
+      <li>商品4</li>
+      <li>商品5</li>
+      <li>商品6</li>
+      <li>商品7</li>
+      <li>商品8</li>
+      <li>商品9</li>
+      <li>商品10</li>
+      <li>商品11</li>
+      <li>商品12</li>
+      <li>商品13</li>
+      <li>商品14</li>
+      <li>商品15</li>
+      <li>商品16</li>
+      <li>商品17</li>
+      <li>商品18</li>
+      <li>商品19</li>
+      <li>商品20</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "./GoodsListItem.vue";
+
+export default {
+  name: "GoodsList",
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    GoodsListItem,
+  },
+};
+</script>
+
+<style>
+.goods-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  padding: 2px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,15 +5,24 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+    ></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import HomeRecommendView from "./childComps/HomeRecommendView";
+import HomeFeatureView from "./childComps/HomeFeatureView";
 
-import { getHomeMultidata } from "@/network/home";
+import NavBar from "@/components/common/navbar/NavBar";
+import TabControl from "@/components/content/tabControl/TabControl";
+import GoodsList from "@/components/content/goods/GoodsList";
+
+import { getHomeMultidata, getHomeGoods } from "@/network/home";
 
 export default {
   name: "Home",
@@ -21,27 +30,78 @@ export default {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        pop: {
+          page: 0,
+          list: [],
+        },
+        new: {
+          page: 0,
+          list: [],
+        },
+        sell: {
+          page: 0,
+          list: [],
+        },
+      },
     };
   },
   components: {
     NavBar,
     HomeSwiper,
     HomeRecommendView,
+    HomeFeatureView,
+    TabControl,
+    GoodsList,
   },
   created() {
     // 1.请求多个数据
-    getHomeMultidata().then((res) => {
-      console.log(res);
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultidata();
+
+    // 2.请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      console.log(this.goods);
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        console.log(res);
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page++;
+      });
+    },
   },
 };
 </script>
 
 <style>
+#home {
+  padding-top: 44px;
+}
+
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 1;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
