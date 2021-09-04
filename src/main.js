c06aa46ac6fb85733730c66f309da4ee44c1afc7
wsh <649318629@qq.com>
2021-09-04 21:29:11 +0800

    xx

--- src/main.js
+++ src/main.js
@@ -4,13 +4,21 @@ import router from './router'
 import store from './store'
 
 import FastClick from 'fastclick'
+import VueLazyLoad from "vue-lazyload"
 
 import toast from './components/common/toast'
 
 Vue.use(toast)
 
 // 解决移动端300ms延迟
-FastClick.attach(document.body)
+FastClick.attach(document.body, {
+  loading: require('@assets/img/common/placeholder.png')
+})
+
+// 使用懒加载
+// 使用时将img中的(:)src替换为v-lazy
+Vue.use(VueLazyLoad)
+
 
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
